d8ec1e38e9fc29eff23b43a90d83ceb275c15041
Ishaq Suleiman <68830069+nitromove@users.noreply.github.com>
2024-01-10 17:15:34 +0100

    Update main.tf

--- dev/ec2_module/main.tf
+++ dev/ec2_module/main.tf
@@ -4,4 +4,14 @@ resource "aws_instance" "webserver" {
   count              = length(var.availability_zone)
   subnet_id          = aws_subnet.vpc_subnets.id
   availability_zone  = var.availability_zone[count.index]
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install -y docker",
+      "sudo usermod -aG docker ec2-user",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+      "sudo yum install -y audible",
+    ]
+  }
 }
